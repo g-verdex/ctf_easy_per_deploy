5cfe556f889b58f3fdf4d7d679e6118b205ee992
d1temnd <74726976+d1temnd@users.noreply.github.com>
2025-03-17 01:17:11 +0700

    setap config

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+class Config:
+    DB_PATH = './containers.db'
+    LEAVE_TIME = 240  # Время жизни контейнера в секундах
+    ADD_TIME = 120  # Время добавления в секундах
+    IMAGE_NAME = "docker images"
+    PORT_IN_CONTAINER = "80"
+    PORT_RANGE = range(9000, 10001)
+    USED_PORTS = set()
+
+config = Config
