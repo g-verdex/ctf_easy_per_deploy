b7b6b18995b1898d1c5d6944ad95264c1c43a886
d1temnd <74726976+d1temnd@users.noreply.github.com>
2025-03-17 14:16:11 +0700

    del files

--- routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-main_routes = Blueprint('main_routes', __name__)
-
-from .deploy import *
-from .stop import *
-from .restart import *
-from .extend import *
-from .main import index
