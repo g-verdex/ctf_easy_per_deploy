a8de52021cb014604a25d28f6fea30d8eab68198
d1temnd <74726976+d1temnd@users.noreply.github.com>
2025-03-17 22:23:07 +0300

    add param for lite start

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 # db_path = os.getenv('DB_PATH')
-leave_time = int(os.getenv('LEAVE_TIME'))
-add_time = int(os.getenv('ADD_TIME'))
-images_name = os.getenv('IMAGES_NAME')
-port_in_container = int(os.getenv('PORT_IN_CONTAINER'))
-PORT_RANGE = range(int(os.getenv('START_RANGE')), int(os.getenv('STOP_RANGE')))
+leave_time = int(os.getenv('LEAVE_TIME', 240))
+add_time = int(os.getenv('ADD_TIME', 120))
+images_name = os.getenv('IMAGES_NAME', "d1temnd/task_images_ozo:1.0.1")
+port_in_container = int(os.getenv('PORT_IN_CONTAINER', '80'))
+PORT_RANGE = range(int(os.getenv('START_RANGE', 9000)), int(os.getenv('STOP_RANGE', 10001)))
 
 
 db_path = './containers.db'
